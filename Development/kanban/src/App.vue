8be34263d8e89ec5cf99673d7cea32672c013d18
LucasPortes26 <99222799+LucasPortes26@users.noreply.github.com>
2022-03-07 20:24:01 -0300

    feat(kanban): Configuração dos botões

--- Development/kanban/src/App.vue
+++ Development/kanban/src/App.vue
@@ -6,20 +6,38 @@
     >
       <v-row>
         <v-col cols="12">
-          <v-btn block class="mx-2 px-4 py-2">
+          <v-btn depressed class="mx-2 px-4 py-2">
+            <v-icon left>
+              mdi-view-dashboard
+            </v-icon>
             Quadros
           </v-btn>
         </v-col>
 
         <v-col cols="12">
-          <v-btn block class="mx-2 px-4 py-2">
+          <v-btn depressed class="mx-2 px-4 py-2">
+          <v-icon left>
+              mdi-forum
+          </v-icon>
             Forum de Disciplinas
           </v-btn>
         </v-col>
 
         <v-col cols="12">
-          <v-btn block class="mx-2 px-4 py-2">
+          <v-btn depressed class="mx-2 px-4 py-2">
+          <v-icon left>
+              mdi-cog
+          </v-icon>
             Configurações
+          </v-btn>
+        </v-col>
+
+        <v-col cols="12" align-self="end">
+          <v-btn depressed class="mx-2 px-4 py-2">
+          <v-icon left>
+              mdi-exit-run
+          </v-icon>
+            Sair
           </v-btn>
         </v-col>
       </v-row>
