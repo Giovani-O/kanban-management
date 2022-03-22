1c5e9c63749b718077cbc48716291a29675ecc9f
LucasPortes26 <99222799+LucasPortes26@users.noreply.github.com>
2022-03-21 21:05:04 -0300

    feat(kanban): Protótipo quadro

--- Development/kanban/src/components/KanbanBoard.vue
+++ Development/kanban/src/components/KanbanBoard.vue
@@ -1,6 +1,13 @@
 <template>
   <v-container>
     <h1>Quadro</h1>
+    <v-card color="transparent" flat class="pa-4">
+      <v-row>
+        <v-col cols="4"></v-col>
+        <v-col cols="4"></v-col>
+        <v-col cols="4"></v-col>
+      </v-row>
+    </v-card>
   </v-container>
 </template>
 
