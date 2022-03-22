291f08236ef989826cfc62006d8bdf47b69e0760
Giovani de Oliveira <50748653+Giovani-O@users.noreply.github.com>
2022-03-21 21:20:03 -0300

    feat(kanban): Inicia protótipo do quadro kanban

--- Development/kanban/src/components/KanbanBoard.vue
+++ Development/kanban/src/components/KanbanBoard.vue
@@ -3,9 +3,56 @@
     <h1>Quadro</h1>
     <v-card color="transparent" flat class="pa-4">
       <v-row>
-        <v-col cols="4"></v-col>
-        <v-col cols="4"></v-col>
-        <v-col cols="4"></v-col>
+        <v-col cols="4">
+          <v-card class="pa-1">
+            <v-card class="activity">
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </p>
+            </v-card>
+            <v-card class="activity">
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </p>
+            </v-card>
+            <v-card class="activity">
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </p>
+            </v-card>
+          </v-card>
+        </v-col>
+
+        <v-col cols="4">
+          <v-card class="pa-1">
+            <v-card class="activity">
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </p>
+            </v-card>
+            <v-card class="activity">
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </p>
+            </v-card>
+          </v-card>
+        </v-col>
+
+        <v-col cols="4">
+          <v-card class="pa-1">
+            <v-card class="activity">
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </p>
+            </v-card>
+          </v-card>
+        </v-col>
       </v-row>
     </v-card>
   </v-container>
@@ -21,3 +68,11 @@
     }
   }
 </script>
+
+<style>
+  .activity {
+    background-color: rgba(255, 255, 255, .2) !important;
+    padding: 12px 12px !important;
+    margin-top: 4px !important;
+  }
+</style>
